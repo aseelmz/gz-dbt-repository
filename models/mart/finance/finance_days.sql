a85131f31faab38eac569c447c240484a11b9f27
Aseel Zaid <zasil3845@gmail.com>
2024-03-06 10:31:19 +0000

    step2

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -9,7 +9,7 @@ SELECT
     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
     ,ROUND(SUM(logcost),0) AS logcost
-    ,ROUND(SUM (CAST(ship_cost AS FLOAT64))) AS ship_cost
+    ,ROUND(SUM (CAST(ship_cost AS FLOAT64))) AS ship_cost 
     ,SUM(quantity) AS quantity
 FROM {{ref("int_orders_operational")}}
 GROUP BY  date_date
